b3595730b9423ca40757e19111a829cf8d414944
Lasse Wolpmann <lassewolpmann@outlook.de>
2022-07-15 15:41:09 +0300

    added car motion data

--- telemetry/packets/motion.py
+++
@@ -1,7 +0,0 @@
-from struct import unpack
-
-
-class CarMotionData:
-    def __init__(self, data):
-        self.data = data
-        self.car_motion_data = unpack('<ffffffHHHHHHffffff', data[24:84])
